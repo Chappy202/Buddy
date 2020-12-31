4f7556de69d1cc729bfc9725557a99f9954cfbf3
JJ <jj@oxibux.com>
2020-12-31 14:57:36 +0200

    Looping Status
    Add random activity messages

--- listeners/ready.js
+++ listeners/ready.js
@@ -1,4 +1,5 @@
 const { Listener } = require('discord-akairo');
+const config = require('../config.js');
 
 module.exports = class ReadyListener extends Listener {
     constructor() {
@@ -11,6 +12,23 @@ module.exports = class ReadyListener extends Listener {
     async exec() {
         this.client.logger.log('info', `${this.client.user.tag} is ready to Rumble!!`);
 
+        // Set first time activity before interval
+        const index = Math.floor(Math.random() * (this.client.config.activities_list.length - 1) + 1);
+        let activity = this.client.config.activities_list[index].split("|");
+        this.client.user.setActivity(`${activity[1]}`, {
+            type: `${activity[0]}`
+        });
+
+        // Change activity every 30 seconds
+        setInterval(() => {
+            const index = Math.floor(Math.random() * (this.client.config.activities_list.length - 1) + 1);
+            let activity = this.client.config.activities_list[index].split("|");
+            this.client.user.setActivity(`${activity[1]}`, {
+                type: `${activity[0]}`
+            });
+        }, 30000);
+
+        this.client.config
         /*const nodes = [...this.client.manager.nodes.values()];
         for (const node of nodes){
             try {
